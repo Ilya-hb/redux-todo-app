3bb0d839a0be4f7f881d7040bfb1d77c31319abf
ilyholu <ilyholu@gmail.com>
2025-06-22 20:15:54 +0300

    LocalStorage integration

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,20 @@
 import { configureStore } from "@reduxjs/toolkit";
 import todoReducer from "./todoSlice";
 
-export default configureStore({
+const saveToLocalStorage = (state) => {
+  try {
+    const serialized = JSON.stringify(state.todos);
+    localStorage.setItem("todos", serialized);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+export const store = configureStore({
   reducer: {
     todos: todoReducer,
   },
 });
+
+// Subscribe for any changes from store
+store.subscribe(() => saveToLocalStorage(store.getState()));
--- src/store/todoSlice.js
+++ src/store/todoSlice.js
@@ -1,8 +1,18 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const getLocalStorageData = () => {
+  try {
+    const todos = localStorage.getItem("todos");
+    return todos ? JSON.parse(todos) : [];
+  } catch (error) {
+    console.log(error.message);
+    return [];
+  }
+};
+
 const todoSlice = createSlice({
   name: "todos",
-  initialState: [],
+  initialState: getLocalStorageData(),
   reducers: {
     addTodo(state, action) {
       console.log(state);
