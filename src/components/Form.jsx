17a5e7678ec0b5a798453c2b2a2233362e3bcada
Ilya <ilyholu@gmail.com>
2025-07-04 14:56:10 +0300

    Feature: delete all completed todos

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,29 +1,24 @@
 import { useState } from "react";
 import { nanoid } from "nanoid";
 import { useDispatch } from "react-redux";
-import { addTodo } from "../store/todoSlice";
+import { addTodo, deleteCompletedTodo } from "../store/todoSlice";
 
 export default function Form() {
   const [inputVal, setInputVal] = useState("");
   const dispatch = useDispatch();
 
   const handleAddTodo = (newTodo) => {
+    if (!inputVal.trim()) return;
+    setInputVal("");
     dispatch(addTodo(newTodo));
   };
 
-  const handleClick = () => {
-    if (!inputVal.trim()) return;
-    handleAddTodo({
-      id: nanoid(),
-      text: inputVal,
-      completed: false,
-      createdAt: new Date(Date.now()).toLocaleDateString("uk-UA"),
-    });
-    setInputVal("");
+  const handleDeleteCompletedTodo = () => {
+    dispatch(deleteCompletedTodo());
   };
 
   return (
-    <div className="flex flex-row gap-5 items-center">
+    <div className="flex flex-col sm:flex-row gap-5 items-center">
       <input
         type="text"
         value={inputVal}
@@ -31,12 +26,28 @@ export default function Form() {
         onChange={(e) => setInputVal(e.target.value)}
         className="px-5 py-2 border-b-[1px] border-neutral-600 focus:outline-none"
       ></input>
-      <button
-        onClick={handleClick}
-        className="px-5 py-2 bg-black border-neutral-700 border-1 rounded-xl hover:bg-neutral-600 transition cursor-pointer"
-      >
-        Create
-      </button>
+
+      <div className="flex  gap-5 items-center">
+        <button
+          onClick={() =>
+            handleAddTodo({
+              id: nanoid(),
+              text: inputVal,
+              completed: false,
+              createdAt: new Date(Date.now()).toLocaleDateString("uk-UA"),
+            })
+          }
+          className="px-5 py-2 bg-black border-neutral-700 border-1 rounded-xl hover:bg-neutral-600 transition cursor-pointer"
+        >
+          Create
+        </button>
+        <button
+          onClick={() => handleDeleteCompletedTodo()}
+          className="px-5 py-2 bg-black border-neutral-700 border-1 rounded-xl hover:bg-neutral-600 transition cursor-pointer"
+        >
+          Delete completed
+        </button>
+      </div>
     </div>
   );
 }
